06a5d314c88581f350daa9127fbebf02f4618b27
Shahid Foy <shahidfoy@gmail.com>
2019-06-12 17:50:22 -0600

    generated wallets

--- test/dev-test.js
+++ test/dev-test.js
@@ -1,7 +1,12 @@
-const Blockchain = require('../blockchain');
+const Wallet = require('../wallet');
+const wallet = new Wallet();
+console.log(wallet.toString());
 
-const bc = new Blockchain();
 
-for (let i = 0; i < 10; i++) {
-    console.log(bc.addBlock(`foo ${i}`).toString());
-}
+// const Blockchain = require('../blockchain');
+
+// const bc = new Blockchain();
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(bc.addBlock(`foo ${i}`).toString());
+// }
